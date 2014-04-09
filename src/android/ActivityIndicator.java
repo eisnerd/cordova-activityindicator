7964b0bc98f1f276e42ae0ce91ab6bdd1450e428
David Eisner <david.eisner@oriel.oxon.org>
2014-04-09 12:13:41 +0100

    Cancellable dialog with notification

--- src/android/ActivityIndicator.java
+++ src/android/ActivityIndicator.java
@@ -2,20 +2,28 @@ package org.apache.cordova.plugin;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 
 public class ActivityIndicator extends CordovaPlugin {
 
 	private ProgressDialog activityIndicator = null;
+	private CallbackContext callbackContext;
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("show")) {
+			this.callbackContext = callbackContext;
 			String text = args.getString(0);
 			show(text);
-			callbackContext.success();
+			PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+			result.setKeepCallback(true);
+			callbackContext.sendPluginResult(result);
 			return true;
 		} else if (action.equals("hide")) {
 			hide();
@@ -31,9 +39,19 @@ public class ActivityIndicator extends CordovaPlugin {
 	 * @param text - Message to display in the Progress Dialog
 	 */
 	public void show(String text) {
+		final CallbackContext callbackContext = this.callbackContext;
+		final ActivityIndicator This = this;
 		ProgressDialog _activityIndicator = activityIndicator;
 		if (_activityIndicator == null)
-			activityIndicator = ProgressDialog.show(this.cordova.getActivity(), "", text);
+			activityIndicator = ProgressDialog.show(this.cordova.getActivity(), "", text, true, true,
+				new DialogInterface.OnCancelListener() {
+					public void onCancel(DialogInterface dialog) {
+						if (This.activityIndicator != null) {
+							This.activityIndicator = null;
+							callbackContext.error("cancel");
+						}
+					}
+				});
 		else
 			_activityIndicator.setMessage(text);
 	}
